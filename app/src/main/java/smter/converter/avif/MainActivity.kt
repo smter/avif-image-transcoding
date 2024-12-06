e552730d366886b41cfdceefb67dcfa7cc869989
smter <smterc@gmail.com>
2024-12-06 11:34:53 +0800

    没动画之前最后的练习

--- app/src/main/java/smter/converter/avif/MainActivity.kt
+++ app/src/main/java/smter/converter/avif/MainActivity.kt
@@ -1,29 +1,87 @@
 package smter.converter.avif
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import smter.converter.avif.ui.theme.AVIF图像转码Theme
 
 class MainActivity : ComponentActivity() {
+    private var selectedImageUri by mutableStateOf<Uri?>(null)
+    private var crfValue by mutableStateOf("28") // 默认CRF值
+    private val pickImageLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                selectedImageUri = result.data?.data
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             AVIF图像转码Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                MyApp()
+//                Scaffold(modifier = Modifier.padding()) { innerPadding ->
+//                    Column(modifier = Modifier.padding(innerPadding)) {
+//                        // 选择图像按钮
+//                        Button(onClick = { /* 选择图像 */
+//                            val intent = Intent(Intent.ACTION_GET_CONTENT)
+//                            intent.type = "image/*"
+//                            pickImageLauncher.launch(intent)
+//                        }) {
+//                            Text("选择图像")
+//                        }
+//
+//                    }
+//                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(modifier: Modifier = Modifier) {
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    val names: List<String> =
+        listOf<String>("谜语说的道理", "哈拿挪fish", "黑狗灭霸别写").plus(List(1000) { "$it" })
+    Surface(
+        modifier = modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        if (shouldShowOnboarding) {
+            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+        } else {
+            LazyColumn(modifier = Modifier.padding(vertical = 16.dp)) {
+                items(items = names) { name ->
+                    Greeting(name = name)
                 }
             }
         }
@@ -32,16 +90,61 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    var expanded by remember { mutableStateOf(false) }
+    val expandedPadding = if (expanded) 48.dp else 0.dp
+    Surface(
+        color = MaterialTheme.colorScheme.primaryContainer,
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = expandedPadding)
+            ) {
+                Text("啊")
+                Text("$name!")
+            }
+            ElevatedButton(onClick = {
+                expanded = !expanded
+            }) {
+                Text(if (expanded) "收起" else "展开")
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 320, heightDp = 640)
 @Composable
 fun GreetingPreview() {
     AVIF图像转码Theme {
-        Greeting("Android")
+        MyApp()
+    }
+}
+
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit, modifier: Modifier = Modifier) {
+
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("欢迎来到OTTO HUB!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ) {
+            Text("哇袄")
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    AVIF图像转码Theme {
+        OnboardingScreen(onContinueClicked = {})
     }
 }

fb7ee98923d04397fea3932c868508916100d7c9
smter <smterc@gmail.com>
2024-12-09 15:53:00 +0800

    ffmpeg-kit默认不支持libaom-av1编码器 所以这里妥协了一下，转换成webp试试 费了半天劲总算是成功转换了，大概是因为SAF不支持java.io.File直接操作的原因卡了很久

--- app/src/main/java/smter/converter/avif/MainActivity.kt
+++ app/src/main/java/smter/converter/avif/MainActivity.kt
@@ -4,13 +4,15 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import android.Manifest
+import android.content.ContentResolver
+import android.content.ContentValues
+import android.provider.DocumentsContract
+import android.provider.MediaStore
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -33,17 +35,23 @@ import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
+import androidx.core.net.toUri
+import androidx.documentfile.provider.DocumentFile
 import com.arthenica.ffmpegkit.FFmpegKit
 import com.arthenica.ffmpegkit.FFmpegKitConfig
 import com.arthenica.ffmpegkit.ReturnCode
 import smter.converter.avif.ui.theme.AVIF图像转码Theme
 import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 import java.util.UUID
+import kotlin.io.path.Path
+import kotlin.io.path.deleteIfExists
 
 class MainActivity : ComponentActivity() {
 
     private var selectedImageUris by mutableStateOf<List<Uri>>(emptyList())
-    private var crfValue by mutableStateOf("28") // 默认CRF值
+    private var crfValue by mutableStateOf("75") // 默认CRF值
 
     //使用可记忆的状态来存储输出目录的路径
     private var outputDir by mutableStateOf("")
@@ -74,7 +82,11 @@ class MainActivity : ComponentActivity() {
     private val selectFolderLauncher =
         registerForActivityResult(ActivityResultContracts.OpenDocumentTree()) { uri ->
             uri?.let {
-                outputDir = it.path ?: "" // 更新 outputDir 状态变量
+                contentResolver.takePersistableUriPermission(
+                    uri,
+                    Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+                )
+                outputDir = uri.toString() ?: "" // 更新 outputDir 状态变量
                 // 可以在这里将路径持久化存储
                 val sharedPrefs = getPreferences(MODE_PRIVATE)
                 val editor = sharedPrefs.edit()
@@ -98,8 +110,24 @@ class MainActivity : ComponentActivity() {
         }
         setContent {
             AVIF图像转码Theme {
-                MainScreen()
+                MyApp(modifier = Modifier)
             }
+        }
+        //检查权限
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            //请求权限
+            val PERMISSIONS_REQUEST_CODE = 1
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                PERMISSIONS_REQUEST_CODE
+            )
+        } else {
+            Toast.makeText(this, "权限已授予", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -125,20 +153,19 @@ class MainActivity : ComponentActivity() {
 
     //主界面
     @Composable
-    @Preview
-    fun MainScreen() {
+    fun MyApp(modifier: Modifier) {
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
             Column(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(16.dp),
+                    .padding(vertical = 16.dp, horizontal = 8.dp),
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Card(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(8.dp)
+                        .padding(vertical = 16.dp, horizontal = 8.dp)
                 ) {
                     OutlinedTextField(
                         value = outputDir,
@@ -146,11 +173,10 @@ class MainActivity : ComponentActivity() {
                         label = { Text("导出文件夹") },
                         readOnly = true,
                         modifier = Modifier
-                            .padding(8.dp)
-                            .clickable { } // 点击触发选择文件夹
+                            .padding(vertical = 16.dp, horizontal = 8.dp)
                     )
                     Button(
-                        modifier = Modifier.padding(8.dp),
+                        modifier = Modifier.padding(vertical = 16.dp, horizontal = 8.dp),
                         colors = androidx.compose.material3.ButtonDefaults.buttonColors(
                             containerColor = MaterialTheme.colorScheme.secondary
                         ),
@@ -159,10 +185,10 @@ class MainActivity : ComponentActivity() {
                 Card(
                     modifier = Modifier
                         .fillMaxSize()
-                        .padding(8.dp)
+                        .padding(vertical = 16.dp, horizontal = 8.dp)
                 ) {
                     Button(
-                        modifier = Modifier.padding(8.dp),
+                        modifier = Modifier.padding(vertical = 16.dp, horizontal = 8.dp),
                         colors = androidx.compose.material3.ButtonDefaults.buttonColors(
                             containerColor = MaterialTheme.colorScheme.secondary
                         ),
@@ -178,12 +204,12 @@ class MainActivity : ComponentActivity() {
                     OutlinedTextField(
                         value = crfValue,
                         onValueChange = { crfValue = it },
-                        label = { Text("CRF 值") },
-                        modifier = Modifier.padding(8.dp)
+                        label = { Text("质量 值") },
+                        modifier = Modifier.padding(vertical = 16.dp, horizontal = 8.dp)
                     )
                     Button(
                         onClick = { convertImages() },
-                        modifier = Modifier.padding(8.dp)
+                        modifier = Modifier.padding(vertical = 16.dp, horizontal = 8.dp)
                     ) {
                         Text("转换")
                     }
@@ -195,37 +221,48 @@ class MainActivity : ComponentActivity() {
     //转码
     private fun convertImages() {
         for (uri in selectedImageUris) {
-            val inputPath = getPathFromURI(uri)
-            val outputFileName = "${UUID.randomUUID()}.avif"
-            val outputPath = File(outputDir, outputFileName).absolutePath
-
+            val inputPath = FFmpegKitConfig.getSafParameterForRead(this, uri)
+            val outputFileName = "${UUID.randomUUID()}.webp"
+            val outputPath = "${cacheDir}/${outputFileName}"
             val command =
-                "-i $inputPath -c:v libaom-av1 -crf $crfValue -b:v 0 $outputPath"
+                "-i $inputPath -q ${crfValue.toInt()} -b:v 0 $outputPath"
             val session = FFmpegKit.execute(command)
 
             if (ReturnCode.isSuccess(session.returnCode)) {
-                // 转换成功，分享结果
+                // 转换成功
+                copyFileToOutputDir(File(outputPath), outputFileName, outputDir.toUri())
+                // 分享结果
                 Toast.makeText(this, "转换成功", Toast.LENGTH_SHORT).show()
-                shareOutput(outputPath)
+                shareOutput(outputPath.toUri())
             } else {
                 // 转换失败，处理错误
-                val message = "转换失败: ${session.failStackTrace}"
+                val message = "转换失败: ${session.logs}"
                 Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
                 println(message)
             }
         }
     }
 
+    // 复制文件到 outputDir
+    private fun copyFileToOutputDir(sourceFile: File, fileName: String, outputDir: Uri) {
+        val contentResolver: ContentResolver = this.contentResolver
+        // 获取 DocumentFile 对象，表示选定的目录
+        val directory = DocumentFile.fromTreeUri(this, outputDir)
+        // 使用 DocumentsContract 创建新文件
+        val newFileUri =
+            directory?.createFile("image/webp", fileName)
+        val outputStream = newFileUri?.let { contentResolver.openOutputStream(it.uri) }
+        if (outputStream != null) {
+            outputStream.write(sourceFile.readBytes())
+            outputStream.close()
+        }
+    }
+
     //分享输出
-    private fun shareOutput(outputPath: String) {
-        val outputUri = FileProvider.getUriForFile(
-            this,
-            "smter.converter.avif.fileprovider",
-            File(outputPath)
-        )
+    private fun shareOutput(outputPath: Uri) {
         val shareIntent = Intent(Intent.ACTION_SEND)
         shareIntent.type = "image/avif"
-        shareIntent.putExtra(Intent.EXTRA_STREAM, outputUri)
+        shareIntent.putExtra(Intent.EXTRA_STREAM, outputPath)
         shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
         startActivity(Intent.createChooser(shareIntent, "分享图像"))
     }
@@ -233,6 +270,12 @@ class MainActivity : ComponentActivity() {
     // 从URI获取文件路径
     private fun getPathFromURI(uri: Uri): String {
         return uri.path ?: ""
+    }
+
+    @Composable
+    @Preview
+    fun preview() {
+        MyApp(modifier = Modifier)
     }
 
 
